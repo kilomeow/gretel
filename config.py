65b82e267c2f197c67f60c74bf62947dd37050aa
kilomeow <dest@disroot.org>
2022-01-26 17:04:14 +0300

    update gretel config

--- config.py
+++ config.py
@@ -1,4 +1,11 @@
-with open('/run/secrets/gretel_config') as cfg:
+import os
+import json
+
+config_path = os.environ('GRETEL_CONFIG_FILE')
+if config_path is None: config_path = '/run/secrets/gretel_config'
+
+with json.load(config_path) as data:
+    data = json.load(cfg)
     mongo_db_url, token = map(lambda s: s.rstrip(' \n'),
                        cfg.readlines()[:2])
     bot_id = int(token.split(":")[0])
