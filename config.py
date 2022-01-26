4fffef7d6610ad893f8fa75d8195e6372fd32d8d
kilomeow <dest@disroot.org>
2022-01-26 17:09:02 +0300

    hotflix

--- config.py
+++ config.py
@@ -5,8 +5,7 @@ config_path = os.environ('GRETEL_CONFIG_FILE')
 if config_path is None: config_path = '/run/secrets/gretel_config'
 
 with json.load(config_path) as data:
-    data = json.load(cfg)
-    mongo_db_url, token = map(lambda s: s.rstrip(' \n'),
-                       cfg.readlines()[:2])
+    mongo_db_url = data['mongo_db']
+    token = data['token']
     bot_id = int(token.split(":")[0])
 
