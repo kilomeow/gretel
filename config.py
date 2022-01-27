03c9e1ee82bb90bd4f030d30dd74e59cf55a1138
Alice Yudina <chimera@chimera.website>
2022-01-27 15:26:12 +0300

    Fixed absolute path + default config name

--- config.py
+++ config.py
@@ -2,10 +2,12 @@ import os
 import json
 
 config_path = os.environ.get('GRETEL_CONFIG_FILE')
-if config_path is None: config_path = '/run/secrets/gretel_config'
 
-with json.load(config_path) as data:
-    mongo_db_url = data['mongo_db']
-    token = data['token']
-    bot_id = int(token.split(":")[0])
-
+if config_path is not None and os.path.exists(config_path):
+    with open(config_path) as file:
+        data = json.load(file)
+        mongo_db_url = data['mongo_db']
+        token = data['token']
+        bot_id = int(token.split(":")[0])
+else:
+    config_path = 'config.json'
