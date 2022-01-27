42ca631d4b4a40743437f6f93a74c72069551171
Alice Yudina <chimera@chimera.website>
2022-01-27 15:50:38 +0300

    Fixed for all paths

--- config.py
+++ config.py
@@ -4,12 +4,12 @@ import json
 config_path = os.environ.get('GRETEL_CONFIG_FILE')
 
 if config_path is not None and os.path.exists(config_path):
-    with open(config_path) as file:
-        data = json.load(file)
+    path = config_path
 else:
-    config_path = 'config.json'
-    data = json.load(config_path)
+    path = "config.json"
 
-mongo_db_url = data['mongo_db']
-token = data['token']
-bot_id = int(token.split(":")[0])
+with open(path) as file:
+    data = json.load(file)
+    mongo_db_url = data['mongo_db']
+    token = data['token']
+    bot_id = int(token.split(":")[0])
